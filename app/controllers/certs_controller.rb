18bcc2e2eda81c4c10e03d8bb5a8bc97370931ff
Duane M. Moody <dmmoody@gmail.com>
2015-03-23 08:24:34 -0700

    add display claimants

--- app/controllers/certs_controller.rb
+++ app/controllers/certs_controller.rb
@@ -8,6 +8,7 @@ class CertsController < ApplicationController
     @group = Group.find(params[:group_id])
     @cert = @group.certs.new(cert_params)
     if @cert.save
+      @claimant = @cert.claimants.create(:first_name => @cert.first_name, :last_name => @cert.last_name, :claimant_number => 1)
       flash[:notice] = "The member was added"
       redirect_to group_path(@cert.group)
     else
