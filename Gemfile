1be0b4171b00d08ded8d360ad15a3b4527d3ae12
Duane M. Moody <dmmoody@gmail.com>
2015-03-25 06:58:41 -0700

    remove user to prepare for devise

--- Gemfile
+++ Gemfile
@@ -9,18 +9,22 @@ gem 'jquery-rails'
 gem 'turbolinks'
 gem 'bootstrap-sass', '~> 3.3.4'
 gem 'bcrypt-ruby', '~> 3.1.5', require: "bcrypt"
+gem 'devise'
 
 group :development do
   gem 'byebug'
   gem 'web-console'
   gem 'spring'
   gem 'quiet_assets'
+  gem 'letter_opener'
 end
 
 group :test, :development do
   gem 'rspec-rails'
+  gem 'factory_girl_rails'
 end
 
 group :test do
   gem 'shoulda-matchers'
+  gem 'capybara'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150324133559) do
+ActiveRecord::Schema.define(version: 20150325135530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,14 +58,6 @@ ActiveRecord::Schema.define(version: 20150324133559) do
     t.float    "amount_received"
     t.date     "date_posted"
     t.text     "reason"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "email"
-    t.string   "password_hash"
-    t.string   "password_salt"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
