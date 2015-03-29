31566bd2d4c657216c4c78f78114747024c50d41
Duane M. Moody <dmmoody@gmail.com>
2015-03-29 08:25:05 -0700

    add edit refunds

--- app/controllers/refunds_controller.rb
+++ app/controllers/refunds_controller.rb
@@ -25,6 +25,22 @@ class RefundsController < ApplicationController
     @refund = Refund.find(params[:id])
   end
 
+  def edit
+    @refund = Refund.find(params[:id])
+    @providers = Provider.all
+    @claimants = Claimant.all 
+  end
+
+  def update
+    @refund = Refund.find(params[:id])
+    if @refund.update(refund_params)
+      flash[:success] = "Refund updated successfully"
+      redirect_to refunds_path
+    else
+      render :edit
+    end
+  end
+
 private
   def refund_params
     params.require(:refund).permit(:provider_id, :claimant_id, :amount_requested, :reason)
